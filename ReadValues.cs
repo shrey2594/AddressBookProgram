3b04409d63cb4ef23986d16c014f25a1e7cf51e3
shrey2594 <shrey2510@gmail.com>
2022-02-27 19:27:07 +0530

    read input values from user

---
+++ ReadValues.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookProgram
+{
+    public static class ReadValues
+    {
+        public static void read()
+        {
+            string firstName;
+            string lastName;
+            string address;
+            string city;
+            string state;
+            int zip;
+            long phone_number;
+            string email;
+            Console.WriteLine("Enter your first name:");
+            firstName = Console.ReadLine();
+            Console.WriteLine("Enter your last name:");
+            lastName = Console.ReadLine();
+            Console.WriteLine("Enter your address:");
+            address = Console.ReadLine();
+            Console.WriteLine("Enter your city:");
+            city = Console.ReadLine();
+            Console.WriteLine("Enter your state:");
+            state = Console.ReadLine();
+            Console.WriteLine("Enter zip code:");
+            zip = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter phone number:");
+            phone_number = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine("Enter your email:");
+            email = Console.ReadLine();
+            Console.WriteLine();
+            AddressBookMain obj = new AddressBookMain(firstName,lastName,address,city,state,zip,phone_number,email);
+            
+        }
+    }
+}
