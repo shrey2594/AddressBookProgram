47a1c2e2a82fbd55201e1c4ae71fc2195fae57b6
shrey2594 <shrey2510@gmail.com>
2022-02-27 19:03:47 +0530

    Default and Parametrized Constructors

---
+++ AddressBookMain.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookProgram
+{
+    public class AddressBookMain
+    {
+        string firstName;
+        string lastName;
+        string address;
+        string city;
+        string state;
+        int zip;
+        long phone_number;
+        string email;
+        public AddressBookMain()
+        {
+            firstName = "Shrey";
+            lastName = "Gupta";
+            address = "113/6 Swaroop Nagar";
+            city = "Kanpur";
+            state = "U.P.";
+            zip = 208002;
+            phone_number = 8376009635;
+            email = "shrey2510@gmail.com";
+        }
+        public AddressBookMain(string name,string lastName,string address,string city,string state,int zip,long contact,string email)
+        {
+            firstName=name;
+            lastName=lastName;
+            address=address;
+            city=city;
+            state=state;
+            zip=zip;
+            phone_number=contact;
+            email=email;
+        }
+    }
+}
